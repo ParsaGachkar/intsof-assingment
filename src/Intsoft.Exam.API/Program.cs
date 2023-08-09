bb568ce961bec85738670d6bbf420f978fe5264e
Parsa Gachkar <ParsaGachkar@Gmail.com>
2023-08-09 23:27:47 +0330

    update Program.cs

--- src/Intsoft.Exam.API/Program.cs
+++ src/Intsoft.Exam.API/Program.cs
@@ -1,6 +1,24 @@
+using Intsof.Exam.Domain.Users;
+using Intsof.Exam.EfCore.DbContext;
+using Intsof.Exam.EfCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region EfCore
+
+builder.Services.AddDbContext<AppDbContext>(q => q.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+#endregion
+
+#region Repositories
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,4 +38,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        if (scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.GetPendingMigrations().Any())
+        {
+            scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
+        }
+
+    }
+}
+
 app.Run();
+
+
+
