148b2663c9474a16d8ecb5349de70b37170112e1
Parsa Gachkar <ParsaGachkar@Gmail.com>
2023-08-09 20:05:56 +0330

    Implement User Repository

---
+++ src/Intsof.Exam.Domain/Users/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Intsof.Exam.Domain.Users;
+
+public interface IUserRepository
+{
+    void Create(User user);
+    Task<User> GetByIdAsync(Guid id);
+    void Update(User user);
+}
---
+++ src/Intsof.Exam.EfCore/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Intsof.Exam.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intsof.Exam.EfCore.Repositories;
+
+public class UserRepository:IUserRepository
+{
+    public DbSet<User> Users { get; }
+
+    public UserRepository(DbSet<User> users)
+    {
+        Users = users;
+    }
+    public async void Create(User user)
+    {
+        await Users.AddAsync(user);
+    }
+
+    public async Task<User> GetByIdAsync(Guid id)
+    {
+        return await Users.FindAsync(id) ?? throw new Exception("User Not Found");
+    }
+
+    public void Update(User user)
+    {
+        Users.Update(user);
+    }
+}
